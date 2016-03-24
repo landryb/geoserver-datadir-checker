72d6a0e8b0e82524f777149506e4fc5bd76a7ef8
Landry Breuil <breuil@craig.fr>
2016-03-24 17:09:33 +0100

    Add parser for SLD files - only checks filesize and sha256 sum for now. Will allow to find duplicates...

---
+++ GSDatadir/SLD.pm
@@ -0,0 +1,30 @@
+use warnings;
+use strict;
+use 5.010;
+use XML::XPath;
+use Digest::SHA;
+
+package GSDatadir::SLD;
+use File::Basename;
+use Data::Dumper;
+
+sub new {
+	my $class = shift;
+	my $file = shift;
+	my $self = {};
+	bless ($self, $class);
+	$self->{file} = $file;
+	$self->{filename} = fileparse($file);
+	$self->{size} = (stat($file))[7];
+	my $sha = Digest::SHA->new(256);
+	$sha->addfile($file);
+	$self->{sha256} = $sha->hexdigest;
+	return $self;
+}
+
+sub dump {
+	my $self = shift;
+	say "SLD: file=$self->{file}, size=$self->{size}, sha1=$self->{sha256}"
+}
+
+1;
---
+++ GSDatadir/SLDCollection.pm
@@ -0,0 +1,37 @@
+use warnings;
+use strict;
+use 5.010;
+
+package GSDatadir::SLDCollection;
+use GSDatadir::SLD;
+use Data::Dumper;
+
+sub new {
+	 my $class = shift;
+	my $path = shift;
+	my $self = {};
+	bless ($self, $class);
+	$self->{path} = $path;
+	$self->{glob} = "$path/{workspaces/*/,}styles/*.sld";
+	$self->{coll} = undef;
+	return $self;
+}
+
+sub list {
+	my $self = shift;
+	foreach (glob "$self->{glob}") {
+		$self->{coll}{$_} = GSDatadir::SLD->new($_);
+	}
+}
+
+sub dump {
+	my $self = shift;
+	my @a = keys %{$self->{coll}};
+	say "SLDCollection: path=$self->{path}, glob=$self->{glob}, ".($#a + 1)." items";
+	foreach (@a) {
+		say "SLD:$_";
+		$self->{coll}{$_}->dump;
+	}
+}
+
+1;
