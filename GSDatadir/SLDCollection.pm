43bd77cb50fd34511bbd5d713152b8aa3746e108
Landry Breuil <breuil@craig.fr>
2016-06-09 10:59:32 +0200

    Implement duplicate detection via sha comparison in SLDCollection, like it was done in VectorDataCollection

--- GSDatadir/SLDCollection.pm
+++ GSDatadir/SLDCollection.pm
@@ -16,4 +16,21 @@ sub new {
 
 sub itemtype { return "GSDatadir::SLD"; }
 
+sub check {
+	my $self = shift;
+	$self->SUPER::check();
+	my %hash_by_sha = ();
+	foreach (keys %{$self->{coll}}) {
+		push @{$hash_by_sha{$self->{coll}{$_}->{sha256}}}, $_;
+	}
+	foreach (keys %hash_by_sha) {
+		my @vd = @{$hash_by_sha{$_}};
+		if ($#vd > 0) {
+			say "same sha256 ($_) for @vd";
+			foreach (@vd) {
+				say "of those, $_ is unreferenced" unless ($self->{coll}{$_}->{referenced_by});
+			}
+		}
+	}
+}
 1;
